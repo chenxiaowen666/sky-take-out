29b47d699c159066e55382607dc8240ad8c6c1a1
xiaowen666 <1361924366@qq.com>
2024-06-02 13:46:50 +0800

    购物车功能（减少按钮功能实现） ===>>> OK

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -22,4 +22,10 @@ public interface ShoppingCartService {
      * 清空购物车商品
      */
     void cleanShoppingCart();
+
+    /**
+     * 删除购物车中一个商品
+     * @param shoppingCartDTO
+     */
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
 }
